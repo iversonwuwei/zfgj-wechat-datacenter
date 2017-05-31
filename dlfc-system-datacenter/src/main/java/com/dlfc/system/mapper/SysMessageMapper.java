739aa1e26fec72b79ae6fa618f189aaa1ec17c0a
hezhiqiang <123456789>
2017-05-31 20:33:20 +0800

    初版

---
+++ dlfc-system-datacenter/src/main/java/com/dlfc/system/mapper/SysMessageMapper.java
@@ -0,0 +1,7 @@
+package com.dlfc.system.mapper;
+
+import com.dlfc.system.entity.SysMessage;
+import com.dlfc.system.entity.SysMessageExample;
+
+public interface SysMessageMapper extends DataMapper<SysMessage, SysMessageExample> {
+}
